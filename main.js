f74eb285a582f04b2b58847052ec70467dfd9335
MasterBlaster3 <josephmaifeld@gmail.com>
2022-05-26 14:10:07 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+//set up camp
+
+const clear = () => {
+    console.log(`clear camp site of debris`);
+}
+
+const tent = () => {
+    console.log(`set up tent`);
+}
+
+const firewood = () => {
+    console.log(`gather firewood`);
+}
+
+const campfire = () => {
+    console.log(`build campfire`);
+}
+
+const dinner = () => {
+    console.log(`make a scrumptious campfire dinner`);
+}
+
+const drink = () => {
+    console.log(`enjoy evening beverages of your choice`);
+}
+
+const sleep = () => {
+    console.log(`get shuteye, try not to think about bears`);
+}
+
+const makeCamp = () => {
+clear() 
+tent()
+firewood()
+campfire()
+dinner()
+drink()
+sleep()
+}
